4184bfc22c758a56f24c97dabc425fd770b1e60c
Spase Mileski <spase.mileski@gmail.com>
2023-10-21 23:17:09 +0200

    new wheel ratios update

--- DS4Windows/DS4Forms/ViewModels/ProfileSettingsViewModel.cs
+++ DS4Windows/DS4Forms/ViewModels/ProfileSettingsViewModel.cs
@@ -861,14 +861,22 @@ namespace DS4WinWPF.DS4Forms.ViewModels
                         index = 3; break;
                     case 450:
                         index = 4; break;
-                    case 720:
+                    case 540:
                         index = 5; break;
-                    case 900:
+                    case 630:
                         index = 6; break;
-                    case 1080:
+                    case 720:
                         index = 7; break;
-                    case 1440:
+                    case 810:
                         index = 8; break;
+                    case 900:
+                        index = 9; break;
+                    case 1080:
+                        index = 10; break;
+                    case 1440:
+                        index = 11; break;
+                    case 1800:
+                        index = 12; break;
                     default: break;
                 }
 
